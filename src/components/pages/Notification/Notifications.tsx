521b20fa48a59b9ec8cbbcc5c8f940afaa74bfd7
Kaushan <kaushandutta5@gmail.com>
2024-02-03 19:49:38 +0530

    fix:files

--- src/components/pages/Notification/Notifications.tsx
+++ src/components/pages/Notification/Notifications.tsx
@@ -1,37 +1,48 @@
-import React,{useEffect,useState} from 'react'
-import ClassElement from '../../ClassElement/ClassElement'
+// Notifications.tsx
+
+import React, { useEffect, useState } from 'react';
+import ClassElement, { Element } from '../../ClassElement/ClassElement';
 import { notification } from '../../../utils/api/Notification/notification';
 import { userData } from '../../../context/AuthContext';
 import AddNotification from '../../popups/AddEvent';
 
-type Notification={
-  _id:string,
-  event:string,
-  description:string,
-  date:string
-}
+type Notification = {
+  course: string;
+  courseId: string;
+  _id: string;
+  collegeId: string;
+  event: string;
+  description: string;
+  date: string;
+};
+
 const Notifications = () => {
-  const {getNotifications}=notification();
-  const [notifications,setNotifications]=useState<Notification>();
-  const {user}=userData();
-  const [popup,setPopup]=useState<boolean>(false);
+  const { getNotifications } = notification();
+  const [notifications, setNotifications] = useState<Notification[]>();
+  const { user } = userData();
+  const [popup, setPopup] = useState<boolean>(false);
 
   useEffect(() => {
-    const loadNotifications=async()=>{
-      setNotifications(await getNotifications(user?.collegeId!));
-    }
+    const loadNotifications = async () => {
+      const notificationsData = await getNotifications(user?.collegeId!);
+      setNotifications(notificationsData);
+    };
     loadNotifications();
-  },[])
+  }, [user]);
+
   return (
     <div className="courses bg-white w-full text-center">
-        
-        <div className="course-subjects">
-          <ClassElement data={notifications} component='notification'/>
-        </div>
-        {user?.entity=='admin' && <button className="other-options w-[200px]" onClick={()=>setPopup(true)}>Add Event</button>}
-        {popup && <AddNotification setPopup={setPopup}/> }
+      <div className="course-subjects">
+        <ClassElement data={notifications || []} component="notification" />
       </div>
-  )
-}
+      {user?.entity === 'admin' && (
+        <button className="other-options w-[200px]" onClick={() => setPopup(true)}>
+          Add Event
+        </button>
+      )}
+      {popup && <AddNotification setPopup={setPopup} />}
+    </div>
+  );
+};
 
-export default Notifications
+export default Notifications;
